0d4056b6c8ba494b5dec7931581f35ca3f9342c6
Rajat Talekar <rajattalekar5143@gmail.com>
2026-06-30 03:52:41 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,9 @@ const adminRoutes = require('./routes/admin.routes');
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173'] : '*',
-  credentials: true,
+  origin: process.env.FRONTEND_URL
+    ? [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174']
+    : '*',
 }));
 app.use(express.json());
 app.use(logger);
